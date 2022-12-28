672ada425ad23c7aaef3f7fdab1e8189e2ccec23
rushabh2390 <69256803+rushabh2390@users.noreply.github.com>
2022-12-28 15:09:14 +0530

    Update admin.py
    update model form to dynamically load form in admin side.

--- languagetranslate/articles/admin.py
+++ languagetranslate/articles/admin.py
@@ -4,30 +4,32 @@ from django import forms
 from django.conf import settings
 import json
 
-support_language = ["title_"+x[0] for x in settings.LANGUAGES]
+support_language = [x[0] for x in settings.LANGUAGES]
 
 
 class ArticleFormAdmin(admin.ModelAdmin):
     model = Article
-    exclude = ('title',)
+    exclude = ('title','description')
 
     def get_fields(self, request, obj=None):
-        fields = super(ArticleFormAdmin, self).get_fields(request, obj)
+        modelFields = [f.name for f in Article._meta.get_fields() if f.name != "id"]
+        fields = [x+"_"+y for x in modelFields for y in support_language]
         if obj:
             new_dynamic_fields = []
-            data = json.loads(obj.title)
-            for key in support_language:
-                if key in data.keys():
-                    new_dynamic_fields.append((key, forms.CharField(
-                        max_length=200, label=key, initial=data[key])))
-                else:
-                    new_dynamic_fields.append((
-                        key, forms.CharField(max_length=200, label=key)))
+            for item in modelFields:
+                data = json.loads(getattr(obj,item))
+                for key in support_language:
+                    if key in data:
+                        new_dynamic_fields.append((item+"_"+key, forms.CharField(
+                            max_length=200, label=item+"_"+key, initial=data[key])))
+                    else:
+                        new_dynamic_fields.append((
+                            item+"_"+key, forms.CharField(max_length=200, label=item+"_"+key)))
 
         else:
             new_dynamic_fields = [
                 (key, forms.CharField(max_length=200, label=key))
-                for key in support_language
+                for key in fields
             ]
         for f in new_dynamic_fields:
             if f[0] not in fields:
@@ -39,14 +41,18 @@ class ArticleFormAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change=False):
         data = form.cleaned_data
+        modelFields = [f.name for f in Article._meta.get_fields() if f.name != "id"]
+
         if change:
             article = self.get_object(request, object_id=obj.id)
-            article.title = json.dumps(data)
-            article.save()
+        
         else:
-            instance = Article()
-            instance.title = json.dumps(data)
-            instance.save()
-
+            article = Article()
+        
+        for item in modelFields:
+            itemvalues = {key.split("_")[1]:data[key] for key in data if key.startswith(item)}
+            setattr(article, item, json.dumps(itemvalues))
+            
+        article.save()
 # Register your models here.
 admin.site.register(Article, ArticleFormAdmin)
